cb7c7de8b1aa2536d7b752cb4f582caa0c47a3a1
“elfacu0” <mahat.mejor@gmail.com>
2023-03-15 08:56:57 -0500

    Add test to make sure a player can't play twice

--- contract/src/test.rs
+++ contract/src/test.rs
@@ -74,9 +74,26 @@ fn test_other_player() {
     let pos_y: u32 = 2;
 
     client.play(&player_a, &pos_x, &pos_y);
-    assert_eq!(client.player_turn(), player_b);
-
     client.play(&player_c, &(pos_x - 1), &(pos_y - 1));
+}
+
+#[test]
+#[should_panic]
+fn test_twice_play() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, GameContract);
+    let client = GameContractClient::new(&env, &contract_id);
+
+    let player_a = Address::random(&env);
+    let player_b = Address::random(&env);
+
+    client.initialize(&player_a, &player_b);
+
+    let pos_x: u32 = 2;
+    let pos_y: u32 = 2;
+
+    client.play(&player_a, &pos_x, &pos_y);
+    client.play(&player_a, &(pos_x - 1), &(pos_y - 1));
 }
 
 #[test]
